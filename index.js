a42b87d7ba09ec51863d263f8dc54b0723134caa
LeeDongGeon <secmatth1996@gmail.com>
2020-07-03 01:42:40 +0900

    linux support

--- index.js
+++ index.js
@@ -128,14 +128,15 @@ function getDownloadTab(){
                     '</div>';
   htmlDivDown = htmlDivDown.replace(/-box_content-/g, contentDownload)
 
-  contentMain = '<a id="download_windows" href="javascript:window.open(\'https://github.com/Cypher-Notepad/Cypher-Notepad/releases/tag/v2.1\')" class="download">\
+  contentMainTemp = '<h2>🎉 Now Available on Linux 🎉</h2>'
+  contentMain = contentMainTemp + '<a id="download_windows" href="javascript:window.open(\'https://github.com/Cypher-Notepad/Cypher-Notepad/releases/tag/v2.1#windows\')" class="download">\
                       <svg height="700px" width="700px" viewBox="0 0 512 499.81" version="1.1" id="windows">\
                         <path d="M0,67.048l207.238-27.137v197.803H0V67.048z M207.238,459.904L0,432.762V262.096h207.238V459.904z M512,\
                         0v237.714H231.619  v-201L512,0z M512,499.81l-280.381-36.714v-201H512V499.81z"></path>\
                       </svg>\
                       <span>Windows</span>\
                     </a>\
-                    <a id="download_linux" href="#" class="download">\
+                    <a id="download_linux" href="javascript:window.open(\'https://github.com/Cypher-Notepad/Cypher-Notepad/releases/tag/v2.1#linux\')" class="download">\
                       <svg height="700px" width="700px" viewBox="0 0 1000 1000" version="1.1" id="linux">\
                         <path d="M369.954 229.402q-6.138 .558 -8.649 5.859t-4.743 5.301q-2.79 .558 -2.79 -2.79 0 \
                         -6.696 10.602 -8.37h5.58zm48.546 7.812q-2.232 .558 -6.417 -3.627t-9.765 -2.511q13.392 \
@@ -224,8 +225,9 @@ function getDownloadTab(){
                  '<div id="dowload_tab_sub_text_detail">' + 
                   '<ul class="download_detail">' + 
                      '<li>Release Date : 28 May 2020</li>' + 
-                     '<li>Requirement : The program requires Java Runtime Environment(JRE) 1.8. You can download JRE1.8 ' + 
-                     '<a href="https://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html">here</a>.</li>' + 
+                     '<li>Requirement : The program requires Java Runtime Environment(JRE) 1.8 or higher. But don\'t worry about it. ' + 
+                     'Our extremely easy guide will lead you.' + 
+                     '</li>' + 
                   '</ul>' + 
                   '</div>' + 
                 '</div>';
